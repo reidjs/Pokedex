75a0b7148c0deb92132136fc5c6873d16a30eacb
reid <reidsherman@gmail.com>
2017-11-15 16:39:16 -0800

    Import router

--- pokedex/frontend/components/root.jsx
+++ pokedex/frontend/components/root.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Provider } from 'react-redux';
 // import App from './app';
 import PokemonIndexContainer from './pokemon/pokemon_index_container';
+import { HashRouter, Route } from 'react-router-dom';
 
 const Root = ({ store }) => (
     <Provider store={store}>
-      <PokemonIndexContainer />
+      <HashRouter>
+        <Route path="/" component={PokemonIndexContainer} />
+      </HashRouter>
     </Provider>
 );
+// <PokemonIndexContainer />
 
 export default Root;
--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Root from './components/root';
-import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
-import { fetchAllPokemon } from './util/api_util';
-import { selectAllPokemon } from './reducers/selectors';
+// import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
+// import { fetchAllPokemon } from './util/api_util';
+// import { selectAllPokemon } from './reducers/selectors';
 import configureStore from './store/store';
+// import { HashRouter, Router } from 'react-router-dom';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
   const store = configureStore();
-  window.dispatch = store.dispatch;
-  window.getState = store.getState;
-  window.receiveAllPokemon = receiveAllPokemon;
-  window.requestAllPokemon = requestAllPokemon;
-  window.fetchAllPokemon = fetchAllPokemon;
-  window.selectAllPokemon = selectAllPokemon;
-  // window.requestAllPokemon = requestAllPokemon;
   ReactDOM.render(<Root store={store}/>, root);
 });
+
+// window.dispatch = store.dispatch;
+// window.getState = store.getState;
+// window.receiveAllPokemon = receiveAllPokemon;
+// window.requestAllPokemon = requestAllPokemon;
+// window.fetchAllPokemon = fetchAllPokemon;
+// window.selectAllPokemon = selectAllPokemon;
+// // window.requestAllPokemon = requestAllPokemon;
